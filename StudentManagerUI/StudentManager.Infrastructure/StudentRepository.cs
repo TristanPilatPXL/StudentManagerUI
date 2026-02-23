a8cf5094a4e97c0ecfbdf9600307d586a7e51b51
TristanPilatPXL <Tristan.Pilat@STUDENT.PXL.BE>
2026-02-23 10:36:08 +0100

    UI-01: 4 lagen test

---
+++ StudentManagerUI/StudentManager.Infrastructure/StudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using StudentManager.Domain;
+
+
+namespace StudentManager.Infrastructure
+{
+    public class StudentRepository
+    {
+        private readonly List<Student> _students = new();
+
+        public void Add(Student student) => _students.Add(student);
+
+        public List<Student> All() => _students;
+    }
+}
